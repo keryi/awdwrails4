9cdcbc58d0ee528c3fff191f4a6736d05ec142f1
keryi <hti_32@yahoo.com>
2015-05-19 06:48:41 +0800

    Use fixtures

--- depot/test/models/product_test.rb
+++ depot/test/models/product_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures :products
+
   test "product attributes must not be empty" do
     product = Product.new
     assert product.invalid?
@@ -48,5 +50,15 @@ class ProductTest < ActiveSupport::TestCase
     bad.each do |name|
       assert new_product(name).invalid?, "#{name} should be invalid"
     end
+  end
+
+  test "product is not valid without a unique title" do
+    product = Product.new title: products(:ruby).title,
+      description: 'A description',
+      price: 7.77,
+      image_url: 'image.png'
+
+    assert product.invalid?
+    assert ['has already been taken'], product.errors[:title]
   end
 end
