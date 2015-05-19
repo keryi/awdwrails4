3dc36c57cf200bb76b86e15e107700de3286dd5e
keryi <hti_32@yahoo.com>
2015-05-19 09:45:21 +0800

    Playtime #Task-D

--- depot/app/controllers/line_items_controller.rb
+++ depot/app/controllers/line_items_controller.rb
@@ -1,6 +1,7 @@
 class LineItemsController < ApplicationController
   include CurrentCart
   before_action :set_cart, only: [:create]
+  before_action :reset_view_count, only: [:create]
   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
 
   # GET /line_items
@@ -74,5 +75,9 @@ class LineItemsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def line_item_params
       params.require(:line_item).permit(:product_id, :cart_id)
+    end
+
+    def reset_view_count
+      session[:counter] = 0 unless session[:counter].nil?
     end
 end
--- depot/app/controllers/store_controller.rb
+++ depot/app/controllers/store_controller.rb
@@ -1,5 +1,17 @@
 class StoreController < ApplicationController
+  before_action :view_count, only: [:index]
+
   def index
     @products = Product.order :title
+  end
+
+  private
+
+  def view_count
+    if session[:counter].nil?
+      session[:counter] = 1
+    else
+      session[:counter] += 1
+    end
   end
 end
