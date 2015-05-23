4a60b5fc724f1ab35c85534de3659a038be27b59
keryi <hti_32@yahoo.com>
2015-05-24 00:02:15 +0800

    Added locale switcher

--- depot/app/controllers/store_controller.rb
+++ depot/app/controllers/store_controller.rb
@@ -5,7 +5,11 @@ class StoreController < ApplicationController
   before_action :view_count, only: [:index]
 
   def index
-    @products = Product.order :title
+    if params[:set_locale]
+      redirect_to store_url(locale: params[:set_locale])
+    else
+      @products = Product.order :title
+    end
   end
 
   private
