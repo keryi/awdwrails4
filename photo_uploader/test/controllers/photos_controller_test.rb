13671bd60fec875167428c17deaa0a10f8e80d0d
keryi <hti_32@yahoo.com>
2015-05-30 00:05:36 +0800

    Make controller tests pass

--- photo_uploader/test/controllers/photos_controller_test.rb
+++ photo_uploader/test/controllers/photos_controller_test.rb
@@ -12,12 +12,28 @@ class PhotosControllerTest < ActionController::TestCase
   end
 
   test "should get create" do
-    get :create
-    assert_response :success
+    photo = photos :one
+    assert_difference('Photo.count') do
+      post :create, photo:
+      {
+        title: photo.title,
+        description: photo.description,
+        uploaded_file: fixture_file_upload('cool.png', 'image/png')
+      }
+    end
+
+    assert_redirected_to photos_path
   end
 
   test "should get destroy" do
-    get :destroy
+    photo = photos :one
+    delete :destroy, id: photo.id
+    assert_redirected_to photos_url
+  end
+
+  test "shoudl get photo" do
+    photo = photos :one
+    get :photo, id: photo.id
     assert_response :success
   end
 
