a2aeddf20121a6d0a1459943620b817391639a50
keryi <hti_32@yahoo.com>
2015-06-01 22:02:31 +0800

    Fixed bad haml, added kaminari for pagination

--- standalone/weibo/db/seeds.rb
+++ standalone/weibo/db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+100.times { |i| Post.create! content: "This is post #{i}" }
